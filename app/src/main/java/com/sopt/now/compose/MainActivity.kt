83c7e73a0560f5d6fb260af38d0ed72641298774
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-04-05 21:48:00 +0900

    [feat] : MainActivity 로직 추가

--- app/src/main/java/com/sopt/now/compose/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/MainActivity.kt
@@ -1,46 +1,147 @@
 package com.sopt.now.compose
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            NOWSOPTAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                val intent = intent
+                val signupId = intent.getStringExtra("signupId") ?: ""
+                val signupPw = intent.getStringExtra("signupPw") ?: ""
+                val signupName = intent.getStringExtra("signupName") ?: ""
+                val signupMbti = intent.getStringExtra("signupMbti") ?: ""
+
+                setContent {
+                    NOWSOPTAndroidTheme {
+                        // A surface container using the 'background' color from the theme
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+
+
+                            MainScreen(signupId, signupPw, signupName, signupMbti)
+                        }
+                    }
                 }
+
             }
-        }
-    }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(signupId: String, signupPw: String, signupName: String, signupMbti: String) {
+Row(
+    modifier = Modifier
+    .fillMaxWidth()
+        .fillMaxHeight()
+    .padding(20.dp)
+    .padding(start = 8.dp)
+       )
+
+{
+    val image: Painter = painterResource(id = R.drawable.profile)
+    Image(painter = image, contentDescription = "Profile Image",
+        modifier = Modifier.padding(start = 8.dp))
+}
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .padding(top = 35.dp)
+            .padding(start = 180.dp)
+
+    ){
+Column(
+    modifier = Modifier
+            .fillMaxHeight()
+)
+{Text(
+    text = "Name",
+    fontSize = 30.sp,
+    color = Color.Black,
+    modifier = Modifier.fillMaxWidth()
+        .padding(bottom = 20.dp)
+)
     Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        text = " $signupName",
+        fontSize = 24.sp,
+        color = Color.Black,
+        modifier = Modifier.fillMaxWidth()
+    )}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NOWSOPTAndroidTheme {
-        Greeting("Android")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(15.dp)
+            .padding(bottom = 150.dp)
+            .padding(start = 20.dp)
+        ,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "ID: $signupId",
+            fontSize = 24.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(8.dp)
+                .padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Password: $signupPw",
+            fontSize = 24.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(8.dp)
+                .padding(bottom = 8.dp)
+        )
+        Text(
+            text = "MBTI: $signupMbti",
+            fontSize = 24.sp,
+            color = Color.Black,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview2() {
+        NOWSOPTAndroidTheme {
+            MainScreen("signupId", "signupPw", "signupName", "signupMbti")
+                }
+    }
