3c7c3b8057ff5ce284e41929fee278babb192c38
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-04-12 01:28:40 +0900

    [ADD] : Constant 생성

---
+++ app/src/main/java/com/sopt/now/compose/Constants/Constant.kt
@@ -0,0 +1,13 @@
+package com.sopt.now.compose.Constants
+
+class Constant {
+    companion object {
+        const val MIN_ID_LENGTH = 6
+        const val MAX_ID_LENGTH = 10
+        const val MIN_PW_LENGTH = 8
+        const val MAX_PW_LENGTH = 12
+        const val MAX_MBTI_LENGTH = 4
+
+    }
+
+}
