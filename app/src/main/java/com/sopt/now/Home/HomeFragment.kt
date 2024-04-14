50636a68702bf9d882243145d1e45051515cf519
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-04-14 15:41:00 +0900

    [Feat] : HomeFragment 로직 구현

---
+++ app/src/main/java/com/sopt/now/Home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.sopt.now.Home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sopt.now.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
