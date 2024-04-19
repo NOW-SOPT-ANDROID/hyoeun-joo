31329190959df3e4163e6679909921c328152cd2
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-04-19 16:37:15 +0900

    [FEAT] : BottomNaviItem 기능구현

--- app/src/main/java/com/sopt/now/compose/BottomNaviItem.kt
+++ app/src/main/java/com/sopt/now/compose/BottomNaviItem.kt
@@ -6,9 +6,9 @@ import com.sopt.now.compose.Const.Companion.MYPAGE
 import com.sopt.now.compose.Const.Companion.SEARCH
 
 sealed class BottomNavItem(
-    val title: Int, val icon: Int, val label: String
+    val title: Int, val icon: Int, val label: String,
 ) {
-    object Home : BottomNavItem(R.string.BottomNavi_home, R.drawable.ic_home_white_24,  HOME)
+    object Home : BottomNavItem(R.string.BottomNavi_home, R.drawable.ic_home_white_24, HOME)
     object Search : BottomNavItem(R.string.BottomNavi_search, R.drawable.ic_search_white_24, SEARCH)
     object MyPage : BottomNavItem(R.string.BottomNavi_myPage, R.drawable.ic_mypage_white_24, MYPAGE)
 }
