27ff001bbe5a95c756293b2456635ca8b2092546
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-05-03 00:58:33 +0900

    [Feat] : SignUpActivity 로직 구현

--- app/src/main/java/com/sopt/now/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/SignUpActivity.kt
@@ -1,13 +1,8 @@
 package com.sopt.now
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
-import com.sopt.now.User.UserDataInput
 import com.sopt.now.api.ServicePool.authService
 import com.sopt.now.databinding.ActivitySignupBinding
 import com.sopt.now.dto.RequestSignUpDto
@@ -15,7 +10,6 @@ import com.sopt.now.dto.ResponseSignUpDto
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
@@ -25,59 +19,6 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
         initViews()
 
-        /*
-                binding.btnSignup2.setOnClickListener {
-                    val signid = binding.etIdSignup.text.toString()
-                    val signpw = binding.etPwSignup.text.toString()
-                    val signname = binding.etNicknameSignup.text.toString()
-                    val signmbti = binding.etMbtiSignup.text.toString()
-
-                    val userData = UserDataInput(signid, signpw, signname, signmbti)
-
-                    when {
-                        signid.isNullOrEmpty() || signpw.isNullOrEmpty() ||
-                                signname.isNullOrEmpty() || signmbti.isNullOrEmpty()
-                        -> {
-                            Toast.makeText(
-                                this,
-                                getString(R.string.sign_up_fail),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-
-                        signid.length !in MIN_ID_LENGTH..MAX_ID_LENGTH -> {
-                            showSnackbar(binding.root, getString(R.string.id_error))
-                        }
-
-                        signpw.length !in MIN_PW_LENGTH..MAX_PW_LENGTH -> {
-                            showSnackbar(binding.root, getString(R.string.pw_error))
-                        }
-
-                        signname.length < MIN_NICKNAME_LENGTH || signname.isBlank() -> {
-                            showSnackbar(binding.root, getString(R.string.nickname_error))
-                        }
-
-                        (signmbti.length > MAX_MBTI_LENGTH) -> {
-                            showSnackbar(binding.root, getString(R.string.mbti_error))
-                        }
-
-                        else -> {
-                            val intent = Intent(this@SignUpActivity, LoginActivity::class.java).apply {
-                                putExtra(LoginActivity.INTENT_USER_DATA, userData)
-
-
-                            }
-                            startActivity(intent)
-
-                            Toast.makeText(
-                                this,
-                                getString(R.string.sign_up_success),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                }
-        */
     }
 
     private fun initViews() {
@@ -94,22 +35,12 @@ class SignUpActivity : AppCompatActivity() {
                 response: Response<ResponseSignUpDto>,
             ) {
                 if (response.isSuccessful) {
-//                    val data: ResponseSignUpDto? = response.body()
                     val userId = response.headers()["location"]
                     Toast.makeText(
                         this@SignUpActivity,
                         "회원가입 성공 유저의 ID는 $userId 입니다",
                         Toast.LENGTH_SHORT,
                     ).show()
-//                    val signUpModel: UserDataInput = UserDataInput(
-//                        binding.etIdSignup.text.toString(),
-//                        binding.etPwSignup.text.toString(),
-//                        binding.etNicknameSignup.text.toString(),
-//                        binding.etMbtiSignup.text.toString()
-//                    )
-
-//                    Log.d("SignUp", "data: $data, userId: $userId")
-
                     finish()
 
                 } else {
@@ -140,23 +71,4 @@ class SignUpActivity : AppCompatActivity() {
             phone = phoneNumber
         )
     }
-
-    companion object {
-        const val MIN_ID_LENGTH = 6
-        const val MAX_ID_LENGTH = 10
-        const val MIN_PW_LENGTH = 8
-        const val MAX_PW_LENGTH = 12
-        const val MIN_NICKNAME_LENGTH = 1
-        const val MAX_MBTI_LENGTH = 4
-
-    }
-
-    fun showSnackbar(view: View, message: String) {
-        Snackbar.make(
-            view,
-            message,
-            Snackbar.LENGTH_SHORT
-        ).show()
-    }
-
 }
