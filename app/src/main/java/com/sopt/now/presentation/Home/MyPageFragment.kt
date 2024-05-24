d6c142259429243c84a4b9433ee0db0dd0992e8f
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-05-24 22:02:17 +0900

    [CHORE] : 코드 정리

---
+++ app/src/main/java/com/sopt/now/presentation/Home/MyPageFragment.kt
@@ -0,0 +1,79 @@
+package com.sopt.now.presentation.Home
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.sopt.now.data.api.ServicePool
+import com.sopt.now.databinding.FragmentMyPageBinding
+import com.sopt.now.data.dto.ResponseUserProfile
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class MyPageFragment : Fragment() {
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding: FragmentMyPageBinding
+        get() = requireNotNull(_binding)
+
+    private var userId: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userId = arguments?.getString("userId")
+        Log.d("MyPageFragment", "userId: $userId")
+        userId?.let { getUserInfo(it) }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun getUserInfo(userId: String) {
+        ServicePool.authService.getUserInfo(userId.toInt())
+            .enqueue(object : Callback<ResponseUserProfile> {
+                override fun onResponse(
+                    call: Call<ResponseUserProfile>,
+                    response: Response<ResponseUserProfile>,
+                ) {
+                    if (response.isSuccessful) {
+                        val userProfile = response.body()
+                        Log.d("MyPageFragment", "User profile: $userProfile")
+
+                        userProfile?.let {
+                            with(binding) {
+                                tvUserId.text = "ID: ${it.data.authenticationId}"
+                                tvUserName.text = "Name: ${it.data.nickname}"
+                                tvPhone.text = "Phone: ${it.data.phone}"
+                            }
+                        }
+                    } else {
+                        onFailure(call, Throwable("Fail: ${response.code()}"))
+                    }
+                }
+                override fun onFailure(call: Call<ResponseUserProfile>, t: Throwable) {
+                    Toast.makeText(requireContext(), t.message.toString(), Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
+    }
+    companion object{
+        const val userId= "userId"
+    }
+}
